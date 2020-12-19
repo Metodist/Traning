b76c258684af3d4db06d7ea12079db2877f5a6ac
Iona M <51836963+Metodist@users.noreply.github.com>
2020-12-19 06:52:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Тренировка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "#bug @Iona_M чини блять";
+            string[] textobt = text.Split(' ');
+
+
+            foreach (string i in textobt)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+            
+        }
+    }
+}
